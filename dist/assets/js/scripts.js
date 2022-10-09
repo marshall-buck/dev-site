82be64dedc8dd76085e8e2f6ac25b88382f1b7ea
Marshall Buck <dev@marshallbuck.com>
2022-10-09 13:13:39 -0500

    deelted dist

--- dist/assets/js/scripts.js
+++
@@ -1,8 +0,0 @@
-// Initialize all tooltips
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-});
-
-// Initialize lightbox
-// const tobii = new Tobii();
